84f2821617bf0ac4759060cdb2772e99f95e19c5
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-08-05 15:24:20 +0700

    Update types.ts

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,11 +1,11 @@
-interface UserCardProps {
+export interface UserCardProps {
   name: string;
   imgUrl: string;
   address: string;
   email: string;
 }
 
-interface UserCardDetailProps {
+export interface UserCardDetailProps {
   email: string;
   address: string;
 }
